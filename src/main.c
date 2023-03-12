c5caf773f506238ad0fa94a5ad83dbd4dedfd3ff
Jim Greer <jbgreer@gmail.com>
2023-03-12 17:04:30 -0500

    added logging

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) 2023 James B. Greer
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#define LOG_MODULE_NAME		app
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	LOG_INF("Hello World! %s\n", CONFIG_BOARD);
 }
